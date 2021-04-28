e8711ff99c6be5e1cb647dcfd8536faaf0a02945
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-04-28 21:29:00 +0530

    Update CurrencyModel.js

--- models/CurrencyModel.js
+++ models/CurrencyModel.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const CurrencySchema = new mongoose.Schema({
-    guildId: String,
-    userId: String,
-    coinsInWallet: Number,
-    coinsInBank: Number,
-    bankSpace: Number
+	guildId: { type: String, required: false },
+	userId: { type: String, required: true },
+	nuggets: { type: Number, default: 0 },
+	fridge: { type: Number, default: 0 },
+	bankSpace: { type: Number, default: 2000 },
+	inventory: [
+		{ type: Object, default: {} },
+	],
+	badges: { type: Array, default: [] },
+	netValue: { type: Number, default: null },
 });
 
 module.exports = mongoose.model('currency', CurrencySchema);
